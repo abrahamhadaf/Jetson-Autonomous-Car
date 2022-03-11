33ab20f1f61534fb2ce873b2d866de375ebacfaf
Abraham <45084728+abrahamhadaf@users.noreply.github.com>
2022-03-11 14:29:42 -0800

    Info on Busses

--- Sample Tests/test_throttle.py
+++ Sample Tests/test_throttle.py
@@ -1,3 +1,9 @@
+# SDA = pin.SDA_1
+# SCL = pin.SCL_1
+# SDA_1 = pin.SDA
+# SCL_1 = pin.SCL
+
+
 from adafruit_servokit import ServoKit
 import board
 import busio
@@ -5,6 +11,8 @@ import time
 from approxeng.input.selectbinder import ControllerResource
     
 print("Initializing Servos")
+# Bus 0 is board SCL_1, SDA_1 in the jetson board definition file
+# Bus 1 is board SCL, SDA in the jetson definition file
 i2c_bus1=(busio.I2C(board.SCL, board.SDA))
 print("Initializing ServoKit")
 kit = ServoKit(channels=16, i2c=i2c_bus1)
